db4f1e412ffd982f4ca670152477d15026479f7f
Odmarken Odmark <Elias.odmarkk@gmail.com>
2024-04-17 21:53:27 +0000

    added colour function/random colour to JS

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+/*--- colour Function ---- */
+
+function getRandomColor() {
+				
+    var letters = "0123456789ABCDEF".split('');
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.round(Math.random() * 15)];
+    } 
+    return color; 
+
+
+} 
