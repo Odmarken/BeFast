3cd4748e2741d16cf249e085a751c10a38796936
Odmarken Odmark <Elias.odmarkk@gmail.com>
2024-04-18 05:58:56 +0000

    adde math random and positions

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,38 @@ function getRandomColor() {
 /*----- Reaction Timmer ---- */
 
 var clickedTime; var createdTime; var reactionTime; 
+
+/*--- Scaling for how fast the boxes should appear --- */
+
+function makeBox() {
+    var time=Math.random();
+    time=time*500;
+
+    setTimeout(function() {
+    
+        if (Math.random()>0.5) {
+        
+            document.getElementById("box").style.borderRadius="100px";
+            
+            } else {
+            
+                document.getElementById("box").style.borderRadius="0";
+            }
+            
+        var top= Math.random();
+            top= top*300;
+        var left= Math.random();
+            left= left*500; 
+            
+        document.getElementById("box").style.top = top + "px";
+        document.getElementById("box").style.left = left + "px"; 
+    
+        document.getElementById("box").style.backgroundColor=getRandomColor();
+    
+        document.getElementById("box").style.display="block";
+        
+        createdTime=Date.now();
+        
+    }, time); 
+
+}
